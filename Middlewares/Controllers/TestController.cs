8be5e46b2bf28fba7bdd00f6e93c6faee3ba5db8
Üsame Delihasan <usamedelihasan14@gmail.com>
2022-09-20 00:20:33 +0300

    Add project files.

---
+++ Middlewares/Controllers/TestController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Middlewares.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+
+        [HttpGet]
+        public int get()
+        {
+            int a = 1;
+            int b = 0;
+
+            int c = a / b;
+
+            return c;
+        }
+    }
+}
